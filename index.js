325e12c5f86c68595eab80f55f0a00ebc791fd7d
Kenneth Phang <bunnyppl@gmail.com>
2019-02-13 18:32:12 +0800

    initial version

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 `use strict`
 
 console.log("Listening to mqtt to lock screen");
-//const { spawn } = require('child_process');
-//var cmd=require('node-cmd');
 const lockSystem = require('lock-system');
 
-//var isWin = process.platform === "win32";
 
 var mqtt = require('mqtt')
 var client  = mqtt.connect({ port: 17353, host: 'm16.cloudmqtt.com', username: 'pvoiqokx', password: 'UEq8zveNkCUN', keepalive: 10000})
@@ -20,14 +17,12 @@ client.on('connect', function () {
 })
 
 client.on('message', function (topic, message) {
-  // message is Buffer
   console.log(process.platform);
   console.log(">>>" + message.toString());
   console.log(">>>" + topic.toString());
   if(topic.toString() === 'lockscreen' &&  message.toString() === 'true'){
 	lockSystem();	
   }
-  //client.end()
 })
 
 
